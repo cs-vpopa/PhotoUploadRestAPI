b9abab5ca81fdae19749b05fe59820b96e422fc3
Vera <cs.info.app@gmail.com>
2024-07-20 15:41:06 +0200

    Initial commit of Spring Boot project

---
+++ photo-server/src/test/java/com/example/photo_server/PhotoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
